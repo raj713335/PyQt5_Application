00b53a6625f5692b6f7975eae0dcfb81c687a5f9
ARNAB DAS <raj713335@gmail.com>
2020-11-27 18:17:11 +0530

    Initial Commit

--- Button_and_Events.py
+++ Button_and_Events.py
@@ -4,34 +4,33 @@ import sys
 
 class MyWindow(QMainWindow):
 
-    def __init__(self):
+    def __int__(self):
         super(MyWindow,self).__init__()
         self.initUI()
 
     def button_clicked(self):
-        self.label.setText("You Pressed Button")
+        self.label.setText("you pressed the button")
         self.update()
 
     def initUI(self):
-        self.setGeometry(200, 200, 500, 500)
-        self.setWindowTitle("Tech with Raj!")
+        self.setGeometry(200, 200, 300, 300)
+        self.setWindowTitle("Tech With Tim")
 
         self.label = QtWidgets.QLabel(self)
-        self.label.setText("My First Label")
-        self.label.move(50, 50)
+        self.label.setText("my first label!")
+        self.label.move(50,50)
 
         self.b1 = QtWidgets.QPushButton(self)
-        self.b1.setText("Click me")
+        self.b1.setText("click me!")
         self.b1.clicked.connect(self.button_clicked)
-
 
     def update(self):
         self.label.adjustSize()
 
 
 def window():
-    app=QApplication(sys.argv)
-    win=MyWindow()
+    app = QApplication(sys.argv)
+    win = MyWindow()
     win.show()
     sys.exit(app.exec_())
 
